a466aa20d120716b29a3b8b289d906f4160c89f4
YourmixJNR <radmakson@gmail.com>
2023-11-05 13:50:43 +0100

    setting the database

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -2,31 +2,30 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
-  const FetchProducts = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/products');
-      if (!response.ok) throw new Error(response.statusText);
-      const data = await response.json();
-    //   setProducts(data);
-    console.log(data)
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
-      // Handle any errors that occur during the data fetching process
-    }
+    const [products, setProducts] = useState([]);
+  
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/products');
+        if (!response.ok) throw new Error(response.statusText);
+        const data = await response.json();
+        // setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        // Handle any errors that occur during the data fetching process
+      }
+    };
+  
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+  
+    return (
+      <div>
+        
+      </div>
+    );
   };
-
-  useEffect(() => {
-    FetchProducts()
-  }, []);
-
-  return(
-    <div>
-
-    </div>
-    
-    ) ;
-};
-
-export default ProductList;
+  
+  export default ProductList;  
