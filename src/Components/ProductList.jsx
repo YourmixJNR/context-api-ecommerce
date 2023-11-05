79c3f788486791779356fcc8cfe1395b8fb13600
YourmixJNR <radmakson@gmail.com>
2023-11-05 14:31:33 +0100

    displaying result

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -9,7 +9,7 @@ const ProductList = () => {
         const response = await fetch('http://localhost:5000/products');
         if (!response.ok) throw new Error(response.statusText);
         const data = await response.json();
-        // setProducts(data);
+        setProducts(data);
         console.log(data);
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -23,7 +23,17 @@ const ProductList = () => {
   
     return (
       <div>
-        
+        {products.map((product, index) => (
+          <div key={index}>
+            <img 
+            src={product.image}
+            alt="Product Image"
+            />
+            <h4>{product.name}</h4>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+          </div>
+        ))}
       </div>
     );
   };
