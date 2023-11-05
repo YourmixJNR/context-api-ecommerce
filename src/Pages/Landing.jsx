c50a333a285f31042778ac40f8cd57b1e12c63a0
YourmixJNR <radmakson@gmail.com>
2023-11-05 12:01:42 +0100

    HeroSection

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,18 +1,9 @@
 import React from "react";
+import HeroSection from "../Components/HeroSection";
 
 const Landing = () => {
   return (
-    <section className="py-5 text-center container">
-      <div className="row py-lg-5">
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">Find Your Next Gem 💎</h1>
-          <p className="lead text-body-secondary">
-             Discover our exquisite collection of products. Find your next favorite
-             item and get ready for a delightful shopping experience.
-          </p>
-        </div>
-      </div>
-    </section>
+    <HeroSection />
   );
 };
 
