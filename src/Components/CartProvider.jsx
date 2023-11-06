c9d3c1e866ee1db6318454e2e2549afedf480c15
YourmixJNR <radmakson@gmail.com>
2023-11-06 21:56:25 +0100

    CartProvider

---
+++ src/Components/CartProvider.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import CartContext from "../Context/CartContext";
+
+const CartProvider = () => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+  const removeItem = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeItem }}>
+        {/* children components */}
+    </CartContext.Provider>
+  )
+};
+
+export default CartProvider;
