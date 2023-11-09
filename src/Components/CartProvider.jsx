ef8be6693eaccfb63ae95909752ca0a9436e6ccc
YourmixJNR <radmakson@gmail.com>
2023-11-09 19:47:08 +0100

    init

--- src/Components/CartProvider.jsx
+++ src/Components/CartProvider.jsx
@@ -7,16 +7,16 @@ const CartProvider = ({children}) => {
 
   const addToCart = (product) => {
     setCart((prevCart) => {
-      const existingItem = prevCart.find((item) => item.id === product.id);
-      if (existingItem) {
-        return prevCart.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      } else {
-        return [...prevCart, { ...product, quantity: 1 }];
-      }
+      // const existingItem = prevCart.find((item) => item.id === product.id);
+      // if (existingItem) {
+      //   return prevCart.map((item) =>
+      //     item.id === product.id
+      //       ? { ...item, quantity: item.quantity + 1 }
+      //       : item
+      //   );
+      // } else {
+      //   return [...prevCart, { ...product, quantity: 1 }];
+      // }
     });
   };
 
