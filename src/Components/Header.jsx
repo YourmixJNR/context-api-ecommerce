c42e84dd2443ef49285e24b71c84d8e47adc1f99
YourmixJNR <radmakson@gmail.com>
2023-11-12 14:27:19 +0100

    init

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div data-bs-theme="dark">
       <div className="navbar navbar-dark bg-dark shadow-sm">
         <div className="container">
-          <h4>Welcome to Our Store</h4>
+          <div>
+            <Link to="/">
+              <h4>Welcome to Our Store</h4>
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart" className="custom-link">
+              <img src="/cart-icon.png" alt="" />
+              <span>Cart</span>
+              <span class="cart-count">3</span>
+            </Link>
+          </div>
         </div>
       </div>
       <Outlet />
