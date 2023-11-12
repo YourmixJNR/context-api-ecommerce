c0d5e0a0d71aa76dbad06022e9e6a245e88f5d32
YourmixJNR <radmakson@gmail.com>
2023-11-12 15:21:50 +0100

    cart display update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import CartContext from "../Context/CartContext";
+import { useState, useEffect, useContext } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+  const { cart } = useContext(CartContext);
+
+  const [cartNumber, setCartNumber] = useState(0);
+
+  useEffect(() => {
+    setCartNumber(cart.length);
+  }, [cart]);
+
   return (
     <div data-bs-theme="dark">
       <div className="navbar navbar-dark bg-dark shadow-sm">
@@ -17,7 +28,7 @@ const Header = () => {
             <Link to="/cart" className="custom-link">
               <img src="/cart-icon.png" alt="" />
               <span>Cart</span>
-              <span class="cart-count">3</span>
+              <span className="cart-count">{cartNumber}</span>
             </Link>
           </div>
         </div>
